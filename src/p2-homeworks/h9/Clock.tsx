87065d763541721409db1b67fafd6d25c17b5a4f
Borislav <borislavizmestiev@gmail.com>
2021-11-22 17:55:34 +0300

    Add HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -7,25 +8,30 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearTimeout(timerId);
     }
     const start = () => {
-        stop()
+        stop();
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
+
         }, 1000)
         setTimerId(id)
+        console.log(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true);
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false);
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    console.log()
+
+    const stringTime = date?.toLocaleTimeString('ru') || <br/> // fix with date
+    const stringDate = date?.toLocaleDateString('ru') // fix with date
 
     return (
         <div>
@@ -36,11 +42,12 @@ function Clock() {
                 {stringTime}
             </div>
 
-            {show && (
-                <div>
+            {show ? (
+                <div className={s.date}>
                     {stringDate}
                 </div>
-            )}
+            ): (<br/>)
+        }
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
